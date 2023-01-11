36b9645f6206a3215025dc826d426e4ab63d1c5e
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2023-01-11 15:57:41 +0000

    Player and grass sprites

--- Pick Up Sticks VS/Main.cpp
+++ Pick Up Sticks VS/Main.cpp
@@ -1,23 +1,86 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Pick Up Sticks", sf::Style::Fullscreen);
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+
+    // --------------------------------------------------
+    // Setup
+    // --------------------------------------------------
+#pragma region Setup
+
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Pick Up Sticks", sf::Style::None);
+
+    srand(time(NULL));
+
+    sf::Texture playerTexture;
+    playerTexture.loadFromFile("Assets/Player_Stand.png");
+    sf::Texture grassTexture;
+    grassTexture.loadFromFile("Assets/Grass.png");
+
+    sf::Sprite playerSprite;
+    playerSprite.setTexture(playerTexture);
+    sf::Sprite grassSprite;
+    grassSprite.setTexture(grassTexture);
+
+    std::vector<sf::Sprite> grassSprites;
+    int numGrassSpritesToAdd = 5;
+    for (int i = 0; i < numGrassSpritesToAdd; ++i)
+    {
+        grassSprite.setPosition(sf::Vector2f(rand() % (window.getSize().x - grassTexture.getSize().x), rand() % (window.getSize().y - grassTexture.getSize().y)));
+        grassSprites.push_back(grassSprite);
+    }
+
+    // Position setup
+    playerSprite.setPosition(sf::Vector2f(0.0f, 100.0f));
+
+#pragma endregion
 
     while (window.isOpen())
     {
+
+        // --------------------------------------------------
+        // Event Polling
+        // --------------------------------------------------
+#pragma region Event Polling
+
         sf::Event event;
         while (window.pollEvent(event))
         {
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Escape)
+                {
+                    window.close();
+                }
+            }
+
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+#pragma endregion
+
+
+        // --------------------------------------------------
+        // Drawing
+        // --------------------------------------------------
+#pragma region Drawing
+
+        window.clear(sf::Color(61, 204, 90));
+
+        // Draw all the things
+        for (int i = 0; i < grassSprites.size(); ++i)
+            window.draw(grassSprites[i]);
+        window.draw(playerSprite);
+
         window.display();
+
+#pragma endregion
+
     }
 
     return 0;
